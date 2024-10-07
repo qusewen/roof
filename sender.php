c8d0ecd4e221a77874b43c0717e6ca7c50fb299c
VItaliy Avseikov <78975759+qusewen@users.noreply.github.com>
2024-10-07 22:31:53 +0300

    Update sender.php

--- sender.php
+++ sender.php
@@ -1,7 +1,8 @@
 <?php
+header('Content-Type: text/html; charset=utf-8; Content-Transfer-Encoding: utf-8; Reply-To:no-reply@example.com';);
     $name = $_POST['name'];
 	$phone = $_POST['phone'];
-    $text = $_POST['text'];
+    	$text = $_POST['text'];
 
 	$to = "housetop.mogilev@gmail.com";
 	$date = date ("d.m.Y");
