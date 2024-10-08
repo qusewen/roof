8f11007199fae3431d5fb6839ec2a0cbb8761823
VItaliy Avseikov <78975759+qusewen@users.noreply.github.com>
2024-10-08 21:19:41 +0300

    Update sender.php

--- sender.php
+++ sender.php
@@ -18,5 +18,11 @@
 	$headers = "From: $from\r\n";
 	$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
 	mail($to, $subject, $msg, $headers);
-
+if (mail($to, $subject, $msg, $headers)) {
+    file_put_contents($log_file, date("Y-m-d H:i:s") . " - Письмо успешно отправлено!\n", FILE_APPEND);
+    echo "Письмо успешно отправлено!";
+} else {
+    file_put_contents($log_file, date("Y-m-d H:i:s") . " - Ошибка отправки письма.\n", FILE_APPEND);
+    echo "Ошибка при отправке письма.";
+}
 ?>
